2211f2c2a751f3c158130b211d6080ddde6fb71a
kazuya kawaguchi <kawakazu80@gmail.com>
2024-01-08 23:10:03 +0900

    docs: add crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,30 @@
-#![warn(rustdoc::missing_crate_level_docs)]
-
+//! The parser for [Unicode Locale Identifiers](https://unicode.org/reports/tr35/#Unicode_locale_identifier)
+//!
+//! ## ⛏️ Conformance
+//!
+//! All code implements of [Unicode UTS #35 Language and Locale Identifiers](https://unicode.org/reports/tr35/#Identifiers).
+//!
+//! ## 🚀 Usages
+//!
+//! ```rust
+//! use unicode_locale_parser::parse_locale_id;
+//!
+//! fn main() {
+//!     // simple language
+//!     let locale = parse_locale_id("ja-JP");
+//!     println!("{:#?}", locale);
+//!
+//!     // language & unicode locale extension
+//!     let locale = parse_locale_id("de-Latn-DE-u-ca-buddhist");
+//!     println!("{:#?}", locale);
+//! }
+//! ```
+//!
+//! ## 🤝 API
+//! - [`parse_locale_id`]: parse [`unicode_locale_id`](https://unicode.org/reports/tr35/#unicode_locale_id)
+//! - [`parse_language_id`]: parse [`unicode_language_id`](https://unicode.org/reports/tr35/#unicode_language_id)
+//! - [`parse_subdivision_id`]: parse [`unicode_subdivision_id`](https://unicode.org/reports/tr35/#unicode_subdivision_id)
+//! - [`parse_measure_unit`]: parse [`unicode_measure_unit`](https://unicode.org/reports/tr35/#unicode_measure_unit)
 mod constants;
 mod extensions;
 mod shared;
